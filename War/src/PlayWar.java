36eba3473e7ea6e17e604a20083dec3f6d2687f4
marika.holz <marika.holz@MU-C1-08.mullen.pvt.k12.co.us>
2021-10-04 11:56:53 -0600

    worked on play war. pull card

--- War/src/PlayWar.java
+++ War/src/PlayWar.java
@@ -1,24 +1,45 @@
 import java.util.ArrayList;
 public class PlayWar
 	{
-	static ArrayList<Card> playerTwoDeck = new ArrayList<Card>();
-	static ArrayList<Card> playerOneDeck = new ArrayList<Card>();
+	static ArrayList<Card> playerTwoHand = new ArrayList<Card>();
+	static ArrayList<Card> playerOneHand = new ArrayList<Card>();
 		public static void main(String[] args)
 			{
+			Deck.fillDeck();
+			Deck.shuffle();
+			dealCards();
+			pullCard();
 			
-			public static void dealCards()
-				{
-				for(int i = 0; i < ((Deck.size)/2); i++)
-					{
-					playerOneDeck.add(Deck.deck.get(0));
-					Deck.deck.remove(0);
-					}
-				}	
-			
-			//each player gets 26 cards
+			//each player gets 26 cards DONE
 			//two face down decks. players pull a card. flip it over. Largest value wins. winner takes both cards
 			//if tie, place another card down each. largest value wins. winner takes all cards
 			//at the end of the game person with all of the cards win
 			}
+		public static void dealCards()
+			{
+			//player one hand
+			for(int i = 0; i < ((Deck.deck.size())/2); i++)
+				{
+				playerOneHand.add(Deck.deck.get(0));
+				Deck.deck.remove(0);
+				}
+			//player two hand
+			for(int i = 0; i < ((Deck.deck.size())/2); i++)
+				{
+				playerTwoHand.add(Deck.deck.get(0));
+				Deck.deck.remove(0);
+				}
+			}	
 
+		public static void pullCard()
+			{
+			for(Card c: playerOneHand)
+				{
+					
+				}
+			for(Card d: playerTwoHand)
+				{
+					
+				}
+			}
 	}
