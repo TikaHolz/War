0d14ccc472c327ec11049857329540c093cfdd40
marika.holz <marika.holz@MU-C1-08.mullen.pvt.k12.co.us>
2021-10-05 11:40:32 -0600

    work9ing on the when won functino

---
+++ War/src/Design.java
@@ -0,0 +1,30 @@
+
+public class Design
+	{
+		public static void name()
+		{
+		 System.out.println("    THE GAME OF WAR");
+	     System.out.println("       __,_____  ");
+	     System.out.println("      / __.==--'   ");
+	     System.out.println("     /#(-'    ");
+	     System.out.println("     `-'   ");
+	         
+		}
+		public static void winner()
+		{
+			System.out.println("                    WINNER!");
+			System.out.println("     .* *.              `o`o`");
+			System.out.println("     *. .*             o`o`o`o     ^,^,^");
+			System.out.println("       *                `o`o`    ^,^,^,^,^");
+			System.out.println("                ***              ^,^,^,^,^");
+			System.out.println("               *****               ^,^,^");
+			System.out.println("                ***");
+	        System.out.println("    ~@~*~@~   ");
+	        System.out.println("  ~*~@~*~@~*~   ");
+	    	System.out.println("  ~*~@smd@~*~                     #$#$#   ");
+	    	System.out.println(" ~*~@~*~@~*~                     #$#$#$#  ");
+	    	System.out.println("    ~@~*~@~                       #$#$#   ");
+
+		}
+
+	}
--- War/src/PlayWar.java
+++ War/src/PlayWar.java
@@ -1,19 +1,46 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 public class PlayWar
 	{
 	static ArrayList<Card> playerTwoHand = new ArrayList<Card>();
 	static ArrayList<Card> playerOneHand = new ArrayList<Card>();
+	
+	static String playerOneName;
+	static String playerTwoName;
+	static int valueOne;
+	static int valueTwo;
+	static boolean won = false;
+	static int playerOneCounter = 0;
+	static int playerTwoCounter = 0;
+	
 		public static void main(String[] args)
 			{
+			Design.name();
+			playersName();
 			Deck.fillDeck();
 			Deck.shuffle();
 			dealCards();
-			pullCard();
 			
+			while(won == false)
+				{
+				pullCardPlayerOne();
+				pullCardPlayerTwo();
+				chooseBiggerValue();
+				if(playerOneCounter == 14)
+					{
+					won = true;
+					Design.winner();
+					}
+				if(playerTwoCounter == 14)
+					{
+					won=true;
+					Design.winner();
+					}
+				}		
 			//each player gets 26 cards DONE
-			//two face down decks. players pull a card. flip it over. Largest value wins. winner takes both cards
-			//if tie, place another card down each. largest value wins. winner takes all cards
-			//at the end of the game person with all of the cards win
+			//two face down decks. players pull a card. flip it over. Largest value wins. winner
+			//if tie, place another card down each. largest value wins. 
+			//at the end of the game person with most points wins
 			}
 		public static void dealCards()
 			{
@@ -30,16 +57,49 @@ public class PlayWar
 				Deck.deck.remove(0);
 				}
 			}	
-
-		public static void pullCard()
+		public static void playersName()
 			{
-			for(Card c: playerOneHand)
+			Scanner userStringInput = new Scanner(System.in);
+			System.out.println("What is player one's name?");
+			playerOneName = userStringInput.nextLine();
+			
+			//player two name
+			System.out.println("What is player two's name?");
+			playerTwoName = userStringInput.nextLine();
+			System.out.println();
+			}
+
+		public static void pullCardPlayerOne()
+			{
+			System.out.println(playerOneName + "'s Card");
+			System.out.println(playerOneHand.get(0).getRank());
+			System.out.println(playerOneHand.get(0).getSuit());
+			valueOne = playerOneHand.get(0).getValue();
+			System.out.println();
+			playerOneHand.remove(0);
+			}
+		public static void pullCardPlayerTwo()
+			{
+			System.out.println(playerTwoName + "'s Card");
+			System.out.println(playerTwoHand.get(0).getRank());
+			System.out.println(playerTwoHand.get(0).getSuit());
+			valueTwo = playerTwoHand.get(0).getValue();
+			playerTwoHand.remove(0);
+			}
+		public static void chooseBiggerValue()
+			{
+			if(valueOne>valueTwo)
 				{
-					
+				System.out.println();
+				System.out.println(playerOneName + " won this round!");
+				playerOneCounter++;	
 				}
-			for(Card d: playerTwoHand)
+			else
 				{
-					
+				System.out.println();
+				System.out.println(playerTwoName + " won this round!");
+				playerTwoCounter++;	
+				System.out.println();
 				}
 			}
 	}
